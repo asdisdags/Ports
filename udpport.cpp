b6edb2f42bfac33d93854590ed670875e73d4a2f
Ásdís Dagsdóttir <asdisdags@gmail.com>
2022-09-23 13:18:38 +0000

    fuuu

--- udpport.cpp
+++ udpport.cpp
@@ -1,4 +1,5 @@
 #include <arpa/inet.h>
+#include <thread>
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <sstream>
@@ -9,39 +10,50 @@
 #include <stdlib.h>
 #include <iostream>
 #include <string.h>
+#include <fcntl.h>
+#include <sys/time.h>
 
 using namespace std;
 
-void scan(int low_port, int high_port, string host){
-    for (int i = low_port; i <= high_port; i++) {
-        int sock = socket(AF_INET, SOCK_DGRAM, 0);
-        if (sock < 0){
-            perror("Failed to create socket");
-        }
+char buffer[4096];
 
-        struct sockaddr_in server_address;
-        server_address.sin_family = AF_INET;
-        server_address.sin_port = htons(i);
+// void scan(int low_port, int high_port, string host){
+//     for (int i = low_port; i <= high_port; i++) {
+//         cout << "Scanning port " << i << endl;
+//         int sock = socket(AF_INET, SOCK_STREAM, 0); 
+//         if (sock < 0){
+//             perror("Failed to create socket");
+//             exit(0);
+//         }
 
-        struct hostent *hp = gethostbyname(host.c_str());
+//         cout << "Socket created" << endl;
+//         struct sockaddr_in server_address;
+//         memset(&server_address, 0, sizeof(server_address));
+
+//         cout << "Server address initialized" << endl;
+//         server_address.sin_family = AF_INET;
+//         server_address.sin_port = htons(i);
+
+//         int set_address = inet_pton(AF_INET, host.c_str(), &server_address.sin_addr);
+//         if (set_address <= 0){
+//             perror("Failed to set socket address");
+//             exit(0);
+//         }
+
+//         cout << "Socket address set" << endl;
         
-        if (hp == 0){
-            perror("Unknown host");
-            exit(1);
-        }
+//         if (connect(sock, (struct sockaddr *)&server_address, sizeof(server_address)) < 0){
+//             cout << "Could not connect to port" << endl;
+//         }
 
-        bcopy((char *)hp->h_addr, (char *)&server_address.sin_addr, hp->h_length);
-        
-        if (connect(sock, (struct sockaddr *)&server_address, sizeof(server_address)) < 0){
-            cout << "Error connecting to port " << i << endl;
-        }
-        else {
-            cout << "Port " << i << " is open" << endl;
-        }
+//         else {
+//             cout << "Port " << i << " is open" << endl;
+//         }
 
-        close(sock);
-    }
-}
+
+//         close(sock);
+//     }
+// }
 
 int main(int argc, char* argv[])
 {   
@@ -51,32 +63,61 @@ int main(int argc, char* argv[])
         exit(1); 
     }
 
-    string host = argv[1];
+    char *host = argv[1];
     int low_port = atoi(argv[2]);
     int high_port = atoi(argv[3]);
-
-    int sockfd;
-
-    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
-    {
-        perror("Failed to create socket"); 
-        exit(1); // eða return (-1)?
-    }
     
-    struct sockaddr_in server_addr;
-    struct hostent *server;
+    // scan all ports from low_port to high_port
+    for (int i = low_port; i <= high_port; i++) {
+        int sockfd;
+        struct sockaddr_in server_address;
 
-    server = gethostbyname(argv[1]);
 
-    if (server == NULL){
-        perror("No such host");
-        exit(0);
+        if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+        {
+            perror("Failed to create socket"); 
+            exit(1); // eða return (-1)?
+        }
+
+        memset(&server_address, 0, sizeof(server_address));
+        server_address.sin_addr.s_addr = inet_addr(host);
+        server_address.sin_family = AF_INET;
+        server_address.sin_port = htons(i);
+
+        if (inet_pton(AF_INET, host, &server_address.sin_addr) <= 0){
+            perror("Failed to set socket address");
+            exit(0);
+        }
+
+        sendto(sockfd, "hello", 5, 0, (struct sockaddr *)&server_address, sizeof(server_address));
+        
+        struct timeval tv;
+        tv.tv_sec = 0;
+        tv.tv_usec = 100000;
+        setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
+        socklen_t socket_len = sizeof(server_address);
+
+        if (recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr *)&server_address, &socket_len) >= 0){
+            cout << "Port " << i << " is open" << endl;
+        }
+        else {
+            cout << "Port " << i << " is closed" << endl;
+        }
+
+        close(sockfd);
     }
 
-    bzero((char *) &server_addr, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(low_port);
-    scan(low_port, high_port, host);
+    // struct hostent *server;
+
+    // server = gethostbyname(argv[1]);
+
+    // if (server == NULL){
+    //     perror("No such host");
+    //     exit(0);
+    // }
+
+    // scan(low_port, high_port, host);
+
     return 0;
 }
     
