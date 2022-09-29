d0d5dc1d5f7ab5ab0ddd7749a997dfe80ea9b8dd
Ásdís Dagsdóttir <asdisdags@gmail.com>
2022-09-29 22:38:34 +0000

    adding scanner

--- scanner.h
+++ scanner.h
@@ -1,42 +1,54 @@
-#ifndef scanner
-#define scanner 
+#ifndef SCANNER_123u488932u9
+#define SCANNER_123u488932u9
 
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <iostream>
-#include <unistd.h>
-#include <sys/types.h>
 #include <set>
+#include <string>
+#include "stdio.h"
+#include "stdlib.h"
 
 using namespace std;
 
-class Scanner{
-    public:
-        int udp_socket;
-        char buffer[1400];
-        struct sockaddr_in server_address;
-        int len;
-        const char* ip;
-        int port_from;
-        int port_to;
-        struct sockaddr_in destination;
-        struct hostent *server;
-        struct timeval tv;
+set<int> scan (char* host, int low_port, int high_port) {
+    char server_message[2048];
+    char client_message[2048];
+    set<int> open_ports;
 
-        Scanner(const char* ip);
+    for (int i = low_port; i <= high_port; i++) {
+        int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
         
-        int open_socket();
-        set<int> scan(int low_port, int high_port);
+        if (sock < 0){
+            perror("Failed to create socket");
+            exit(1);
+        }
 
-    private:
-        int udp_sock;
-        set<int> ports;
+        memset(server_message, '\0', sizeof(server_message));
+        memset(client_message, '\0', sizeof(client_message));
 
+        // set up server address
+        struct sockaddr_in server_address;
+        int sock_addr_len = sizeof(server_address);
+        server_address.sin_family = AF_INET;
+        server_address.sin_port = htons(i);
+        server_address.sin_addr.s_addr = inet_addr(host);
+        
+        // set timeout for recvfrom() - if no response before timeout, port is closed
+        struct timeval tv;
+        tv.tv_sec = 0;
+        tv.tv_usec = 100000;
+        setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, (const void*)&tv, sizeof(tv));
+        
+        if (sendto(sock, client_message, strlen(client_message), 0, (struct sockaddr*)&server_address, sizeof(server_address)) < 0){
+            perror("Failed to send message");
+        }
+        if (recvfrom(sock, server_message, sizeof(server_message), 0, (struct sockaddr*)&server_address, (socklen_t*)&sock_addr_len) >= 0){
+            cout << "Port " << i << " is open" << endl;
+            open_ports.insert(i);
+        }
+       
+        close(sock);
+    }
+
+    return open_ports;
 };
-
 
 #endif
